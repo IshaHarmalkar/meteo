b3d23b5abf515c0a752c3a62920182ba7dfc475f
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2024-05-19 00:07:24 +0530

    Injected the forecast HTML form JS

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let humidityElement = document.querySelector("#humidity");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000) ;
-let iconElement = document.querySelector("#icon");
-              
-   iconElement.innerHTML= `<img  src="${response.data.condition.icon_url}"  class="weather-app-icon" ></img>`;
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img  src="${response.data.condition.icon_url}"  class="weather-app-icon" ></img>`;
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
@@ -20,25 +20,27 @@ let iconElement = document.querySelector("#icon");
   timeElement.innerHTML = formatDate(date);
 
   console.log(response.data);
-
 }
 
-function formatDate(date)
-{
+function formatDate(date) {
   let minutes = date.getMinutes();
   let hour = date.getHours();
-  let days = ["Sunday", "Monday", "Tuesdday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesdday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   let day = days[date.getDay()];
-  if(minutes < 10)
-    {
-      minutes = `0${minutes}`;
-    }
-
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
   return `${day} ${hour}:${minutes},`;
-
 }
 
 function searchCity(city) {
@@ -56,7 +58,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml = forecastHtml +
+    `   
+  <div class="weather-forecast-day">
+    <div class="weather-forecaste-date">${day}</div>
+    <div class="weather-forecaste-icon">⛅</div>
+    <div class="weather-forecast-temperature">
+     <div class="weather-forecast-temperature">
+      <strong>15°</strong>
+     </div>
+    <div class="weather-forecast-temperature">9°</div>
+    </div> 
+ </div>
+ `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+displayForecast();
